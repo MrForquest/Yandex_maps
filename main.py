498ea832a9cb6c4b73ab37457b5588ce1aee6189
MrForquest <folp228@gmail.com>
2022-02-03 22:40:10 +0300

    Задача 3(ГОТОВО)

--- main.py
+++ main.py
@@ -32,26 +32,43 @@ screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
     nz = z
+    ncoordinates = coordinates
     keys = pygame.key.get_pressed()
-
+    ncoordinates = [float(i) for i in ncoordinates.split(",")]
     if keys[pygame.K_s]:
         nz -= 1
         nz = max(nz, 0)
-    elif keys[pygame.K_w]:
+    if keys[pygame.K_w]:
         nz += 1
         nz = min(nz, 17)
-
-    if nz != z:
-        z = nz
+    if nz != 0:
+        if keys[pygame.K_UP]:
+            ncoordinates[1] += 10000 * (1 / nz) ** 5
+        if keys[pygame.K_DOWN]:
+            ncoordinates[1] -= 10000 * (1 / nz) ** 5
+        if keys[pygame.K_LEFT]:
+            ncoordinates[0] -= 10000 * (1 / nz) ** 5
+        if keys[pygame.K_RIGHT]:
+            ncoordinates[0] += 10000 * (1 / nz) ** 5
+    ncoordinates = ",".join([str(i) for i in ncoordinates])
+    if nz != z or ncoordinates != coordinates:
         map_params = {
             "l": "map",
-            "ll": coordinates,
-            "z": z
+            "ll": ncoordinates,
+            "z": nz
         }
         map_api_server = "http://static-maps.yandex.ru/1.x/"
-        os.remove(map_file)
+
         response = requests.get(map_api_server, params=map_params)
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(response.url)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            continue
+        z = nz
+        coordinates = ncoordinates
         map_file = "map.png"
+        os.remove(map_file)
         with open(map_file, "wb") as file:
             file.write(response.content)
         screen.fill("black")
